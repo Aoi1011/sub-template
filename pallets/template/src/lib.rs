8779fa824a8f80df2b7b81f29f39a5fd8c209f54
Aoi <ht.so.ik0102@gmail.com>
2021-04-18 21:02:15 +0700

    modify lib.rs

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,28 +1,45 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{decl_storage, decl_module, StorageValue, StorageMap, dispatch, ensure};
-use frame_system::{ensure_signed};
+use frame_support::{
+	decl_module, decl_storage, dispatch::DispatchResult, 
+	codec::{Decode, Encode},
+};
+use frame_system::ensure_signed;
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct Kitty <Hash, Balance> {
+	id: Hash,
+	dna: Hash,
+	price: Balance,
+	gen: u64,
+}
 
 pub trait Trait: frame_system::Config {}
 
 decl_storage! {
 	trait Store for Module<T: Trait> as KittyStorage {
 		// Declare storage and gettter function here.
-		Value: map hasher(blake2_128_concat) T::AccountId => u64;
+		OwnedMyKitties get(fn get_my_kitties): map hasher(blake2_128_concat) T::AccountId => Kitty<T::Hash, T::Balance>;
 	}
 }
 
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 
-		// declare public functions here. 
+		// declare public functions here.
 		#[weight = 10_000]
-		fn set_value(origin, value: u64) -> dispatch::DispatchResult {
-			let _sender = ensure_signed(origin)?;
+		fn create_kitty(origin, number: u32) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
 
-			// ensure!(!<Value<T>>::contains_key(value), "Key already exists!");
+			let new_kitty = Kitty {
+				id: Hash::hash_of(&0),
+				dna: Hash::hash_of(&0),
+				price: <T::Balance as As<u64>>::sa(0),
+				gen: 1,
+			};
 
-			<Value<T>>::insert(_sender, value);
+			<OwnedMyKitties<T>>::insert(sender, new_kitty);
 
 			Ok(())
 		}
